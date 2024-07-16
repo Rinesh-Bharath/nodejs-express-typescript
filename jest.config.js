6a4f6b8e38247a601d9e7cbabe1c93dff79bfa98
Rinesh Bharath <rinesh.bha@batchservice.com>
2024-07-16 19:46:33 +0530

    chore: implement test coverage in jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/src/repository',
+    '/src/shared',
+  ],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/src/tests/**/*.spec.ts'],
