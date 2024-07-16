069e7d3690f549423a53dc88d25d6f346d0cab62
Rinesh Bharath <rinesh.bha@batchservice.com>
2024-07-16 11:43:32 +0530

    feat: add super test for http testing

--- src/tests/user.spec.ts
+++ src/tests/user.spec.ts
@@ -1,7 +1,27 @@
 import { describe, expect, test } from '@jest/globals';
+const request = require('supertest');
+import app from '../app';
 
-describe('sum module', () => {
+const userId = '01J2KSMRYFVVR4G72WK5ZS9QJA';
+
+describe('Sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(3).toBe(3);
+  });
+});
+
+describe('User related endpoints ', () => {
+  it('Ping the web server', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+  });
+
+  it('GET /user should respond with a user object', async () => {
+    const response = await request(app).get(`/user?userId=${userId}`);
+    expect(response.status).toBe(200);
+    const { status: statusCode, data } = response.body;
+    const { userId: id } = data;
+    expect(statusCode).toEqual(200);
+    expect(id).toEqual(userId);
   });
 });
