d710e0267a5a5c205d3c078bbac0d476368f353d
Rinesh Bharath <rinesh.bha@batchservice.com>
2024-07-16 18:34:37 +0530

    fix: disable logging for tests

--- src/shared/logger/server.ts
+++ src/shared/logger/server.ts
@@ -2,15 +2,24 @@ import pino from 'pino';
 
 const destination = `${__dirname}/../../../logs/server.log`;
 
+const { NODE_ENV } = process.env as Readonly<{
+  NODE_ENV: string;
+}>;
+
+// Supress logs for testing
+const logLevel = NODE_ENV === 'test' ? 'silent' : 'info';
+
 const serverLogger = pino({
   timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
   transport: {
     targets: [
       {
+        level: logLevel,
         target: 'pino/file',
         options: { destination: destination, mkdir: true },
       },
       {
+        level: logLevel,
         target: 'pino-pretty',
         options: { colorize: true },
       },
