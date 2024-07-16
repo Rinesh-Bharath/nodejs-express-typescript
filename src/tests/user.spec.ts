bcf3ece666c1f3b1d6bd7c2a2dd56cf75ecd170c
Rinesh Bharath <rinesh.bha@batchservice.com>
2024-07-16 19:56:01 +0530

    test: improve test cases

--- src/tests/user.spec.ts
+++ src/tests/user.spec.ts
@@ -3,7 +3,13 @@ import { describe, expect } from '@jest/globals';
 import app from '../app';
 import { connectDB, disConnectDB } from '../repository/mongodb/init';
 
-const userId = '01J2KSMRYFVVR4G72WK5ZS9QJA';
+let mockUserId: string;
+
+const mockUser = {
+  email: 'email@abc.com',
+  firstName: 'firstname',
+  lastName: 'lastname',
+};
 
 beforeAll(async () => {
   await connectDB();
@@ -13,18 +19,46 @@ afterAll(async () => {
   await disConnectDB();
 });
 
-describe('User related endpoints ', () => {
+describe('GET /', () => {
   it('Ping the web server', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
   });
+});
 
-  it('GET /user should respond with a user object', async () => {
-    const response = await request(app).get(`/user?userId=${userId}`);
+describe('POST /user', () => {
+  it('Create a user should respond with a user', async () => {
+    const response = await request(app).post('/user').send(mockUser);
+    expect(response.status).toBe(200);
+    const { status: statusCode, data: user } = response.body;
+    expect(statusCode).toEqual(200);
+    const { userId, email } = user;
+    mockUserId = userId;
+    expect(email).toEqual(mockUser.email);
+  });
+});
+
+describe('GET /user', () => {
+  it('Missing userId should respond with 404', async () => {
+    const response = await request(app).get(`/user`);
+    expect(response.status).toBe(404);
+    const { status: statusCode } = response.body;
+    expect(statusCode).toEqual(404);
+  });
+
+  it('Invalid userId should respond with 404', async () => {
+    const response = await request(app).get(`/user?userId=abc`);
+    expect(response.status).toBe(404);
+    const { status: statusCode } = response.body;
+    expect(statusCode).toEqual(404);
+  });
+
+  it('Valid userId should respond with a user', async () => {
+    const response = await request(app).get(`/user?userId=${mockUserId}`);
     expect(response.status).toBe(200);
     const { status: statusCode, data } = response.body;
-    const { userId: id } = data;
     expect(statusCode).toEqual(200);
-    expect(id).toEqual(userId);
+    const { userId } = data;
+    expect(userId).toEqual(mockUserId);
   });
 });
