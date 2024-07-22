54684e05889428d9e5738442c44b271cbbe0610d
Rinesh Bharath <rinesh.bha@batchservice.com>
2024-07-22 21:13:34 +0530

    chore: remove unused import

--- src/core/user/service.ts
+++ src/core/user/service.ts
@@ -2,10 +2,9 @@ import { ulid } from 'ulid';
 
 import { connectDB } from '../../repository/mongodb/init';
 import { IUser, User } from '../../repository/mongodb/user';
-import { errorLogger as logger } from '../../shared/logger';
-import { CreateUserRequest, ReadUserRequest } from './interface';
 import { CustomError } from '../../shared/error/custom-error';
 import { Status } from '../../shared/error/status.enum';
+import { CreateUserRequest, ReadUserRequest } from './interface';
 
 export async function createUserService(request: CreateUserRequest): Promise<IUser> {
   try {
