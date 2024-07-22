6675e1c984578a504b024f532d9f40efb0777e9b
Rinesh Bharath <rinesh.bha@batchservice.com>
2024-07-22 21:06:07 +0530

    fix: custom error to utilize status enums

--- src/shared/error/custom-error.ts
+++ src/shared/error/custom-error.ts
@@ -1,10 +1,10 @@
 import { Status } from './status.enum';
 
 export class CustomError extends Error {
-  public status: number;
+  public status: Status;
   public data?: any;
 
-  constructor(status: number, message: string, data?: any) {
+  constructor(status: Status, message: string, data?: any) {
     super(message);
     this.status = status;
     this.data = data;
