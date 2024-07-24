f92747d0cd12e56df15c3bf7a5190665b6036738
Rinesh Bharath <rinesh.bha@batchservice.com>
2024-07-24 18:52:31 +0530

    chore: extend the options for k6 tests

--- k6-tests/script.js
+++ k6-tests/script.js
@@ -1,15 +1,28 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const userId = '01J35TJPAZREQNZ9CKYTTT1870';
+const serverUrl = 'http://localhost:4020';
 
 export const options = {
-  vus: 100,
-  duration: '30s',
+  scenarios: {
+    user_get: {
+      exec: 'userget',
+      // name of the executor to use
+      executor: 'constant-vus',      
+      // executor-specific configuration
+      vus: 50,
+      duration: '30s',
+      // env specific config
+      tags: { 
+        name: 'UserGetUrl' 
+      },
+    }
+  }
 };
 
-export default function () {
-  let res = http.get(`http://localhost:4020/user?userId=${userId}`);
+export function userget() {
+  const userId = '01J35TJPAZREQNZ9CKYTTT1870';
+  let res = http.get(`${serverUrl}/user?userId=${userId}`);
   check(res, { 'Post status is 200': r => r.status === 200 });
   sleep(0.3);
 }
