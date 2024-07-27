1dee20214391d8e5d7c7c0fd7429701724aaea03
Rinesh Bharath <rinesh.bha@batchservice.com>
2024-07-27 21:17:55 +0530

    test: optimize user test file

--- src/tests/api/user.spec.ts
+++ src/tests/api/user.spec.ts
@@ -1,11 +1,13 @@
 import request from 'supertest';
+import { ulid } from 'ulid';
 
 import { faker } from '@faker-js/faker';
 import { describe, expect } from '@jest/globals';
 
-import app from '../app';
-import { connectDB, disConnectDB } from '../repository/mongodb/init';
-import { IUser } from '../repository/mongodb/user';
+import app from '../../app';
+import { connectDB, disConnectDB } from '../../repository/mongodb/init';
+import { IUser } from '../../repository/mongodb/user';
+import { Status } from '../../shared/error/status.enum';
 
 // Faker JS API reference
 // https://fakerjs.dev/api/
@@ -16,7 +18,7 @@ const lastName = faker.person.lastName(sex);
 const email = faker.internet.email({ firstName, lastName });
 
 const mockUser: Partial<IUser> = {
-  userId: faker.string.uuid(),
+  userId: ulid(),
   email: email,
   firstName: firstName,
   lastName: lastName,
@@ -33,16 +35,16 @@ afterAll(async () => {
 describe('GET /', () => {
   it('Ping the web server', async () => {
     const response = await request(app).get('/');
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(Status.OK);
   });
 });
 
 describe('POST /user', () => {
   it('Create a user should respond with a user', async () => {
     const response = await request(app).post('/user').send(mockUser);
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(Status.OK);
     const { status: statusCode, data: user } = response.body;
-    expect(statusCode).toEqual(200);
+    expect(statusCode).toEqual(Status.OK);
     const { userId, email } = user;
     mockUser.userId = userId;
     expect(email).toEqual(mockUser.email);
@@ -50,25 +52,25 @@ describe('POST /user', () => {
 });
 
 describe('GET /user', () => {
-  it('Missing userId should respond with 404', async () => {
+  it('Missing userId should respond with BadRequest', async () => {
     const response = await request(app).get(`/user`);
-    expect(response.status).toBe(404);
+    expect(response.status).toBe(Status.BadRequest);
     const { status: statusCode } = response.body;
-    expect(statusCode).toEqual(404);
+    expect(statusCode).toEqual(Status.BadRequest);
   });
 
-  it('Invalid userId should respond with 404', async () => {
+  it('Invalid userId should respond with BadRequest', async () => {
     const response = await request(app).get(`/user?userId=abc`);
-    expect(response.status).toBe(404);
+    expect(response.status).toBe(Status.BadRequest);
     const { status: statusCode } = response.body;
-    expect(statusCode).toEqual(404);
+    expect(statusCode).toEqual(Status.BadRequest);
   });
 
   it('Valid userId should respond with a user', async () => {
     const response = await request(app).get(`/user?userId=${mockUser.userId}`);
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(Status.OK);
     const { status: statusCode, data } = response.body;
-    expect(statusCode).toEqual(200);
+    expect(statusCode).toEqual(Status.OK);
     const { userId } = data;
     expect(userId).toEqual(mockUser.userId);
   });
